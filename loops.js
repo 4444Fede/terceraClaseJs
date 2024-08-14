46dd615bd5a373b73654c59e44da7ebd81fac2ab
4444Fede <federico.vega@gm2dev.com>
2024-08-14 10:00:32 -0300

    feat(loops.js): add prettier format

--- loops.js
+++ loops.js
@@ -146,129 +146,70 @@ function separateOddAndEvenFrom1to20() {
   return `Hay ${countEvens} pares del 1 al 20 y son: ${evenNumbers} \nHay ${countOdds} impares del 1 al 20 y son: ${oddNumbers}`;
 }
 
-function calculatePromedio(grades){
-  let i = 0
-  let promedio = 0
-  while(i < grades.length){
-    promedio += grades[i]
-    i++
+function calculatePromedio(grades) {
+  let i = 0;
+  let promedio = 0;
+  while (i < grades.length) {
+    promedio += grades[i];
+    i++;
   }
-  return promedio/i
+  return promedio / i;
 }
 
 let usuarios = ["juan", "maria", "admin", "carlos"];
 let usuarioBuscado = "admin";
 
-function verificarUsuario(usernames, searchedUser){
-  let i = 0
-  let found = false
-  while(i < usernames.length){
+function verificarUsuario(usernames, searchedUser) {
+  let i = 0;
+  let found = false;
+  while (i < usernames.length) {
     if (searchedUser === usernames[i]) {
-      found = true
+      found = true;
     }
-    i++
+    i++;
   }
-  return found ? 'Usuario encontrado' : 'Usuario no encontrado'
+  return found ? "Usuario encontrado" : "Usuario no encontrado";
 }
- 
-let contraseñas = ["password", "1234567", "supersecret", "contraseña123"]; 
+
+let contraseñas = ["password", "1234567", "supersecret", "contraseña123"];
 function verificarContrasenias(passwords) {
-  let i = 0
-  let passwordsEigthLetters = 0
-  while(i < passwords.length){
+  let i = 0;
+  let passwordsEigthLetters = 0;
+  while (i < passwords.length) {
     if (passwords[i].length > 8) {
-      passwordsEigthLetters++
+      passwordsEigthLetters++;
     }
-    i++
+    i++;
   }
-  return `${passwordsEigthLetters} contraseñas que tienen 8 letras`
+  return `${passwordsEigthLetters} contraseñas que tienen 8 letras`;
 }
 
 let correos = ["correo1@example.com", "correo2@example", "correo3@example.com"];
 function verificarCorreo(argument) {
-  let validMails = 0
-  let i = 0
-  while(i<correos.length){
-    if (correos[i].includes('@') && correos[i].includes('.')) {
-      validMails++
+  let validMails = 0;
+  let i = 0;
+  while (i < correos.length) {
+    if (correos[i].includes("@") && correos[i].includes(".")) {
+      validMails++;
     }
-    i++
+    i++;
   }
-  return validMails === correos.length ? 'Todos los correos son validos' : 'Correo invalido encontrado'
+  return validMails === correos.length
+    ? "Todos los correos son validos"
+    : "Correo invalido encontrado";
 }
 
-function isNumberPrime(number){
-  let i = 1
-  let isPrime = true
+function isNumberPrime(number) {
+  let i = 1;
+  let isPrime = true;
   if (number < 0) {
-    return 'Un numero negativo no puede ser primo'
+    return "Un numero negativo no puede ser primo";
   }
-  do{
-    i++
-    if (number%i === 0 && i !== number) {
-      isPrime = false
+  do {
+    i++;
+    if (number % i === 0 && i !== number) {
+      isPrime = false;
     }
-  }while(i<number)
-  return isPrime
+  } while (i < number);
+  return isPrime;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
